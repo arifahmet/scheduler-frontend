09e12cfb98619181f14732feab9334e6f45f9df3
Arif Ahmet <arifahmetbarbak@gmail.com>
2021-02-25 03:38:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {CookiesProvider} from 'react-cookie';
+import ListTasks from './ListTasks'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <CookiesProvider>
+                <Router>
+                    <Switch>
+                        <Route path='/' exact={true} component={Home}/>
+                        <Route path='/tasks' exact={true} component={ListTasks}/>
+                    </Switch>
+                </Router>
+            </CookiesProvider>
+        )
+    }
 }
 
 export default App;
