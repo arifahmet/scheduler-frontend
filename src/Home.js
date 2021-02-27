2cf1c72d70cfe6f39769898b63035690bbacdbcd
Arif Ahmet <arifahmetbarbak@gmail.com>
2021-02-27 16:45:00 +0300

    change bug

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import './App.css';
 import AppNavbar from './AppNavbar';
-import { Button, Container } from 'reactstrap';
-import { withCookies } from 'react-cookie';
+import {Button, Container} from 'reactstrap';
+import {withCookies} from 'react-cookie';
 
 class Home extends Component {
     state = {
         isLoading: true,
         isAuthenticated: false,
-        user: undefined
+        user: undefined,
+        csrfToken: undefined
     };
 
     constructor(props) {
@@ -21,9 +22,14 @@ class Home extends Component {
     }
 
     async componentDidMount() {
-        const response = await fetch('https://barbaktech-scheduler-backend.herokuapp.com/api/v1/user', {credentials: 'include'});
+        const response = await fetch('https://barbaktech-scheduler-backend.herokuapp.com/api/v1/user', {
+            method: 'GET',
+            credentials: 'include',
+            mode: 'cors'
+        });
         console.log(response);
         const body = await response.text();
+        console.log(body);
         if (body === '' || response.status !== 200) {
             this.setState(({isAuthenticated: false}))
         } else {
@@ -36,8 +42,10 @@ class Home extends Component {
     }
 
     logout() {
-        fetch('https://barbaktech-scheduler-backend.herokuapp.com/api/v1/user/logout', {method: 'POST', credentials: 'include',
-            headers: {'X-XSRF-TOKEN': this.state.csrfToken}}).then(res => res.json())
+        fetch('https://barbaktech-scheduler-backend.herokuapp.com/api/v1/user/logout', {
+            method: 'POST', credentials: 'include',
+            headers: {'X-XSRF-TOKEN': this.state.csrfToken}
+        }).then(res => res.json())
             .then(response => {
                 window.location.href = response.logoutUrl + "?id_token_hint=" +
                     response.idToken + "&post_logout_redirect_uri=" + window.location.origin;
