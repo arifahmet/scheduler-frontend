06b2373e7fbd7f533ab6b9a1a548af1c2937031d
Arif Ahmet <arifahmetbarbak@gmail.com>
2021-03-01 01:32:02 +0300

    change versions

--- src/config.js
+++ src/config.js
@@ -1,19 +1,17 @@
 const CLIENT_ID = process.env.CLIENT_ID;
 const ISSUER = process.env.ISSUER;
 const BACKEND_URL = process.env.BACKEND_URL;
+const REDIRECT_URL = process.env.REDIRECT_URL || 'http://localhost:3000/implicit/callback';
 const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
 
 export default {
   oidc: {
     clientId: CLIENT_ID,
     issuer: ISSUER,
-    redirectUri: 'http://localhost:3000/implicit/callback',
+    redirectUri: REDIRECT_URL ,
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
-  },
-  resourceServer: {
-    messagesUrl: 'http://localhost:3000/api/messages',
   },
   backend: BACKEND_URL
 };
