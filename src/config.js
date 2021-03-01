c78e966a2f6d284f8943e2db5380a1638156713e
Arif Ahmet <arifahmetbarbak@gmail.com>
2021-03-02 02:04:37 +0300

    add param

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const CLIENT_ID = process.env.CLIENT_ID;
 const ISSUER = process.env.ISSUER;
 const BACKEND_URL = process.env.BACKEND_URL;
-const REDIRECT_URL = process.env.REDIRECT_URL;
+const REDIRECT_URL = process.env.REDIRECT_URL || 'https://scheduler-frontend-phi.vercel.app/implicit/callback';
 const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
 
 export default {
